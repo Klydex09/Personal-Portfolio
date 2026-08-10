50fbcd915fc8c459250b63fa68446fe41d13b8d4
Klydex09 <jamesklydexhonor@gmail.com>
2026-08-10 12:01:33 +0800

    Update schedule.js

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -2,7 +2,6 @@ export const weeklySchedule = {
   sunday: [
     { start: "2:00 AM", end: "12:00 PM", title: "REST DAY", type: "rest" },
     { start: "12:00 PM", end: "9:00 PM", title: "NO CLASS", type: "free" },
-    { start: "9:00 PM", end: "11:59 PM", title: "HOME", type: "home" }
   ],
 
   monday: [
@@ -12,18 +11,15 @@ export const weeklySchedule = {
     { start: "5:30 PM", end: "6:30 PM", title: "ART APPRECIATION", type: "school" },
     { start: "6:30 PM", end: "7:30 PM", title: "SCIENCE, TECHNOLOGY, AND SOCIETY", type: "school" },
     { start: "7:30 PM", end: "9:00 PM", title: "INFORMATION MANAGEMENT", type: "school" },
-    { start: "9:00 PM", end: "11:59 PM", title: "SLEEP AT WORK", type: "sleep" }
   ],
 
   tuesday: [
     { start: "2:00 AM", end: "11:00 AM", title: "WORK — CONCENTRIX", type: "work" },
-    { start: "12:00 PM", end: "1:30 PM", title: "OOP", type: "school" },
+    { start: "12:00 PM", end: "1:30 PM", title: "OBJECT-ORIENTED-PROGRAMMING", type: "school" },
     { start: "1:30 PM", end: "3:00 PM", title: "VACANT", type: "free" },
     { start: "3:00 PM", end: "5:30 PM", title: "PLATFORM TECHNOLOGIES", type: "school" },
     { start: "5:30 PM", end: "6:30 PM", title: "THE LIFE AND WORK OF RIZAL", type: "school" },
-    { start: "6:30 PM", end: "7:30 PM", title: "WST", type: "school" },
-    { start: "7:30 PM", end: "9:00 PM", title: "VACANT", type: "free" },
-    { start: "9:00 PM", end: "11:59 PM", title: "SLEEP AT WORK (OPTIONAL)", type: "sleep" }
+    { start: "6:30 PM", end: "7:30 PM", title: "WEB-SYSTEM AND TECHNOLOGIES", type: "school" },
   ],
 
   wednesday: [
@@ -34,18 +30,15 @@ export const weeklySchedule = {
     { start: "5:30 PM", end: "6:30 PM", title: "ART APPRECIATION", type: "school" },
     { start: "6:30 PM", end: "7:30 PM", title: "SCIENCE, TECHNOLOGY, AND SOCIETY", type: "school" },
     { start: "7:30 PM", end: "9:00 PM", title: "INFORMATION MANAGEMENT", type: "school" },
-    { start: "9:00 PM", end: "11:59 PM", title: "SLEEP AT WORK", type: "sleep" }
   ],
 
   thursday: [
     { start: "2:00 AM", end: "11:00 AM", title: "WORK — CONCENTRIX", type: "work" },
-    { start: "12:00 PM", end: "1:30 PM", title: "OOP", type: "school" },
+    { start: "12:00 PM", end: "1:30 PM", title: "OBJECT-ORIENTED-PROGRAMMING", type: "school" },
     { start: "1:30 PM", end: "3:00 PM", title: "VACANT", type: "free" },
     { start: "3:00 PM", end: "5:30 PM", title: "PLATFORM TECHNOLOGIES", type: "school" },
     { start: "5:30 PM", end: "6:30 PM", title: "THE LIFE AND WORK OF RIZAL", type: "school" },
-    { start: "6:30 PM", end: "7:30 PM", title: "WST", type: "school" },
-    { start: "7:30 PM", end: "9:00 PM", title: "VACANT", type: "free" },
-    { start: "9:00 PM", end: "11:59 PM", title: "SLEEP AT WORK (OPTIONAL)", type: "sleep" }
+    { start: "6:30 PM", end: "7:30 PM", title: "WEB-SYSTEM AND TECHNOLOGIES", type: "school" },
   ],
 
   friday: [
