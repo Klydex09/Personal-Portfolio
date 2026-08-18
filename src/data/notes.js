873b6b35071aa5015c97f593eccebb17ae6174d5
K9Kaine <jamesklydexhonor@gmail.com>
2026-08-18 20:48:39 +0800

    Update notes.js

--- src/data/notes.js
+++ src/data/notes.js
@@ -7,11 +7,67 @@ export const notes = [
     content: "Past is past. Learn from experiences, keep things simple, move forward, and make every choice count. Safety First."
   },
   {
+    id: "HCI",
+    title: "HCI Midterm II",
+    description: "Review material for HCI studies. 2nd Year BSIT",
+    category: "Human-Computer-Interaction",
+    type: "PDF",
+    file: "/media/notes/HCI/HCI - User Needs.pdf"
+  },
+  {
+    id: "HCI",
+    title: "HCI Midterm I",
+    description: "Review material for HCI studies. 2nd Year BSIT",
+    category: "Human-Computer-Interaction",
+    type: "PDF",
+    file: "/media/notes/HCI/HCI - PERSONAS.pdf"
+  },
+   {
+    id: "P.E",
+    title: "PathFit II Midterm",
+    description: "Review material: Topic: Volleyball. 2nd Year BSIT",
+    category: "PathFit",
+    type: "PDF",
+    file: "/media/notes/PathFit/PATHFIT_3_Sports_Midterm_Module.pdf"
+  },
+  {
     id: 2,
     title: "Career Direction",
     date: "2025-02-22",
     category: "Career",
     content: "Finish BSIT, improve technical skills, gain experience, and eventually build a stable career in the technology industry."
+  },
+  {
+    id: "IM",
+    title: "Information Management I",
+    description: "Review material for SQL and Database. 2nd Year BSIT",
+    category: "IM",
+    type: "PDF",
+    file: "/media/notes/Information Managemnt/IM 1,1.pdf"
+  },
+  {
+    id: "IM",
+    title: "Information Management II",
+    description: "Review material for SQL and Database. 2nd Year BSIT",
+    category: "IM",
+    type: "PDF",
+    file: "/media/notes/Information Managemnt/IM 1.2.pdf"
+  },
+   {
+    id: "Arts",
+    title: "Arts Appreciation",
+    description: "Review material for Arts. 2nd Year BSIT",
+    category: "Arts",
+    type: "PDF",
+    file: "/media/notes/Arts/Humanities_Arts_Appreciation_Midterm.pdf"
+  },
+  {
+    id: "Arts",
+    title: "Arts Appreciation",
+    description: "Review material for Arts. 2nd Year BSIT",
+    category: "Arts",
+    type: "PDF",
+    file: "/media/notes/STS/STS_Indigenous_to_Good_Life.pdf"
   },
   {
     id: "rizal-review-er",
